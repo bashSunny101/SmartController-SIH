535dd33a6ca30b9e41ca372ec14e0f6f3089df8a
Sunny Pal <sunny2212102@akgec.ac.in>
2023-12-12 17:41:33 +0530

    login updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <div className="flex-col p-8 text-center">
-      <div className="flex flex-col items-center mb-6">
+      <div className="flex flex-col items-center mb-11">
         <img
           src="src\assets\image (4).svg"
           alt="logo"
@@ -14,8 +14,10 @@ function App() {
         <div className="text-white">Tagline</div>
       </div>
       <div className="flex">
-        <div className="border-2 rounded-xl p-4 flex-grow mr-5">hello</div>
-        <div>
+        <div className=" p-4 flex-grow"></div>
+        <div
+    className="inline-block h-auto w-0.5 self-stretch bg-gray-500 opacity-100 dark:opacity-50"></div>
+        <div className="ml-20">
           <Login />
         </div>
       </div>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,35 +3,34 @@ import React from 'react'
 function Login() {
   return (
     <>
-        <div className="rounded-xl border bg-card text-card-foreground shadow w-[90vw] max-w-xl">
+        <div className="rounded-xl border-2 border-gray-700 bg-card text-card-foreground shadow w-[90vw] max-w-xl">
         <div className="flex flex-col space-y-1.5 p-6 text-center">
-          <h3 className="font-semibold leading-none tracking-tight text-gray-400">Login</h3>
-          <p className="text-sm text-muted-foreground text-gray-400">
+          <h3 className="font-semibold leading-none tracking-tight text-gray-300">Login</h3>
+          <p className="text-sm text-muted-foreground text-gray-300">
             Get all energy solutions at one place...
           </p>
         </div>
         <div className="p-6 pt-0">
           <form className="space-y-8">
-            <div className="space-y-2">
+            <div className="space-y-2 text-left">
               <label
-                className="text-sm text-gray-400 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                className="text-sm  text-gray-300 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 for=":R5lbala:-form-item"
               >
                 Username
               </label>
               <input
                 className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-gray-700"
-                placeholder="admin@aicte.gov.in"
+                placeholder="username"
                 id=":R5lbala:-form-item"
                 aria-describedby=":R5lbala:-form-item-description"
                 aria-invalid="false"
                 name="username"
-                value="admin@aicte.gov.in"
               />
             </div>
-            <div className="space-y-2">
+            <div className="space-y-2 text-left">
               <label
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-400"
+                className="text-sm  font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-300"
                 for=":R9lbala:-form-item"
               >
                 Password
@@ -39,16 +38,15 @@ function Login() {
               <input
                 type="password"
                 className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-gray-700"
-                placeholder="admin@aicte"
+                placeholder="password"
                 id=":R9lbala:-form-item"
                 aria-describedby=":R9lbala:-form-item-description"
                 aria-invalid="false"
                 name="password"
-                value="admin@aicte"
               />
             </div>
             <button
-              className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary bg-white text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 text-black"
+              className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary bg-gray-50 text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 text-black"
               type="submit"
             >
               Login
