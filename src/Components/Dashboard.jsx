d5c6fa96cdc62a4660a37a073f6e96b07918c808
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2023-12-14 16:36:07 +0530

    fixed nav

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -11,7 +11,7 @@ function Dashboard() {
     <>
       <div>
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex flex-wrap items-center justify-between mx-auto p-4 nav">
             <a
               href="/"
               className="flex items-center space-x-3 rtl:space-x-reverse"
