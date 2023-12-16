1bc97b134c9c82cdd2da2a11d2a4368d5d9ef9db
Sunny Pal <sunny2212102@akgec.ac.in>
2023-12-16 16:34:12 +0530

    added component

--- src/Components/ComMonDetails.jsx
+++ src/Components/ComMonDetails.jsx
@@ -7,8 +7,8 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer,
 } from "recharts";
+
 const backendOptions = [
   "Solar Plant",
   "Wind Plant",
@@ -18,132 +18,146 @@ const backendOptions = [
 
 const data = [
   {
-    name: "Page A",
+    name: "Jan",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
+    name: "Feb",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "Mar",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
+    name: "Apr",
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
+    name: "May",
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
+    name: "Jun",
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  // Add more data as needed
 ];
 
 function ComMonDetails() {
   return (
-    <>
-        <div className="border-gray-800 flex justify-center items-center h-screen">
-          <div className="container">
-            <div className="m-2 flex justify-between  text-2xl">
-              Power Generation Components
-              <div className="flex">
-                <select
-                  id="countries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                >
-                  <option selected="all">All</option>
-                  {backendOptions.map((option, index) => (
-                    <option key={index} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-                <select
-                  id="countries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                >
-                  <option selected>Last 24 hours</option>
-                  <option value="CA">last 7 days</option>
-                  <option value="FR">last 30 days</option>
-                </select>
-                ``
-              </div>
-              {/* dropdown */}
-            </div>
-            <div className="container ">
-              <LineChart
-                width={1270}
-                height={500}
-                data={data}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <XAxis dataKey="name" />
-
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="pv"
-                  stroke="#8884d8"
-                  activeDot={{ r: 8 }}
-                />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-              </LineChart>
-              <table className="min-w-full ">
-                <thead>
-                  <tr>
-                    <th className="border p-3">Source Type</th>
-                    <th className="border p-3">Energy Generated</th>
-                    <th className="border p-3">Active Component Sites</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="border p-3">1</td>
-                    <td className="border p-3">2</td>
-                    <td className="border p-3">3</td>
-                  </tr>
-                </tbody>
-              </table>
-              {/* <ResponsiveConta iner width="100%" height="100%"> */}
-              {/* </ResponsiveContainer> */}
-            </div>
+    <div className="flex flex-col items-center justify-center h-full">
+      {/* First Container */}
+      <div className="container m-2">
+        <div className="text-2xl">
+          Power Generation Components
+        </div>
+        <div className="flex justify-between text-2xl">
+          <div className="flex">
+            {/* Select components */}
           </div>
-          <div className="container">
-            <div className="m-2 flex justify-between  text-2xl">
-              Power Generation Components
-            </div>
-          </div>
+        </div>
+        <div className="container">
+          <LineChart
+            width={500}
+            height={500}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+          <table className="min-w-full">
+            <thead>
+              <tr>
+                <th className="border p-3">Source Type</th>
+                <th className="border p-3">Energy Generated</th>
+                <th className="border p-3">Active Component Sites</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Dummy data for the first table */}
+              <tr>
+                <td className="border p-3">Solar Plant</td>
+                <td className="border p-3">5000 MW</td>
+                <td className="border p-3">15</td>
+              </tr>
+              <tr>
+                <td className="border p-3">Wind Plant</td>
+                <td className="border p-3">3200 MW</td>
+                <td className="border p-3">12</td>
+              </tr>
+              <tr>
+                <td className="border p-3">Hydroelectric Plant</td>
+                <td className="border p-3">4300 MW</td>
+                <td className="border p-3">8</td>
+              </tr>
+              {/* Add more rows as needed */}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </>
+
+      {/* Second Container */}
+      <div className="container m-2">
+        <div className="text-2xl">
+            Component List
+        </div>
+        <table className="min-w-full">
+            <thead>
+              <tr>
+                <th className="border p-3">Component ID</th>
+                <th className="border p-3">Name</th>
+                <th className="border p-3">Type</th>
+                <th className="border p-3">Maximum Capacity</th>
+                <th className="border p-3">Location</th>
+                <th className="border p-3">Addition Properties</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Dummy data for the second table */}
+              <tr>
+                <td className="border p-3">SP101</td>
+                <td className="border p-3">Sunshine Array</td>
+                <td className="border p-3">Solar</td>
+                <td className="border p-3">1000 MW</td>
+                <td className="border p-3">California</td>
+                <td className="border p-3">High efficiency</td>
+              </tr>
+              <tr>
+                <td className="border p-3">WP202</td>
+                <td className="border p-3">Breeze Turbine</td>
+                <td className="border p-3">Wind</td>
+                <td className="border p-3">500 MW</td>
+                <td className="border p-3">Texas</td>
+                <td className="border p-3">Low maintenance</td>
+              </tr>
+              {/* Add more rows as needed */}
+            </tbody>
+          </table>
+      </div>
+    </div>
   );
 }
 
